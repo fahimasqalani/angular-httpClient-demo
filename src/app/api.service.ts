65c15fc3eaf64a53d796890218a02aba879f23a5
unknown <fahim.asqalani@beyondsoft.com>
2020-02-06 11:33:48 +0800

    add comment

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,6 +8,7 @@ export class ApiService {
   API_KEY = 'd883d37f3ee44639ad56d7620e22bdc0';
   constructor(private httpClient: HttpClient) { }
 
+//function to fetch data from api and use to distribute outside using HttpClient 
   public getNews(){
     return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
   }
